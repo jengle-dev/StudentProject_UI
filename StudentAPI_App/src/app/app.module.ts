bfe3058a37764133ed6be1d67737f1c45c431c52
Engle, Jennifer <jennifer.engle@hrblock.com>
2025-02-23 17:52:06 -0600

    angular files

---
+++ StudentAPI_App/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { StudentComponent} from "./student/student.component";
+import { ShowStudentComponent } from "./student/show-student/show-student.component";
+import { AddEditStudentComponent } from "./student/add-edit-student/add-edit-student.component";
+
+import { HttpClientModule } from '@angular/common/http';
+import { SharedService } from './shared.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-department/show-department.component';
+import { AddEditDepComponent } from './department/add-edit-department/add-edit-department.component';
+
+@NgModule({
+    declarations: [
+      AppComponent,
+      StudentComponent,
+      ShowStudentComponent,
+      AddEditStudentComponent,
+      DepartmentComponent,
+      ShowDepartmentComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule
+    ],
+    providers: [SharedService],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
